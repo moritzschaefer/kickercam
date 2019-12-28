834840a60aab300ecda3c3dcaa23ca61303b0090
Moritz Schaefer <mail@moritzs.de>
2019-12-28 21:44:28 +0100

    fix labels from 6800-7200

--- v2/labeler.py
+++ v2/labeler.py
@@ -1,3 +1,4 @@
+import sys
 import time
 
 import cv2
@@ -7,7 +8,7 @@ import pandas as pd
 
 pos = None
 go_back = None
-delay = 25
+delay = 200
 
 
 def mousePosition(event,x,y,flags, passed_param):
@@ -21,24 +22,34 @@ def mousePosition(event,x,y,flags, passed_param):
         delay *= 1.7
 
 
-def label(filename):
+def label(filename, start_frame=0, ball_pos=[]):
+
     global pos, go_back, delay
 
     # If the input is the camera, pass 0 instead of the video file name
     cap = cv2.VideoCapture(filename)
 
     # Check if camera opened successfully
-    if cap.isOpened() == False: 
+    if cap.isOpened() == False:
         print("Error opening video stream or file")
 
     cv2.namedWindow('Frame')
 
     cv2.setMouseCallback('Frame', mousePosition)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
 
-    ball_pos = []
+    ret, frame = cap.read()
+    cv2.imshow('Frame', frame)
+    cv2.waitKey(1600)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+
     # Read until video is completed
     while(cap.isOpened()):
         frame_pos = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+
+        if frame_pos % 50 == 0:
+            print(frame_pos)
+
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret is not True:
@@ -83,5 +94,11 @@ def label(filename):
     return ball_pos
 
 if __name__ == '__main__':
-    ball_pos = label('output.mp4')
+    try:
+        start_frame = int(sys.argv[1])
+    except IndexError:
+        start_frame = 0
+    ball_pos = pd.read_csv('label.csv').apply(lambda row: (row['x'], row['y']), axis=1).tolist()
+    ball_pos = label('output.mp4', start_frame, ball_pos)
+
     pd.DataFrame({'x': [v[0]  if v else None for v in ball_pos], 'y': [v[1]  if v else None for v in ball_pos]}).to_csv('label.csv')
--- v2/show_labels.py
+++ v2/show_labels.py
@@ -1,15 +1,22 @@
+import math
+
 import cv2
 import pandas as pd
 
+RESIZE = 1  # set to 1 to disable scaling
+SHOW_BALL_LABEL = True
 
+# 6800 -> 7250
 def main(video_fn, label_fn):
+
+    delay = 25
     cap = cv2.VideoCapture(video_fn)
 
     # Check if camera opened successfully
     if cap.isOpened() == False: 
         print("Error opening video stream or file")
 
-    cv2.namedWindow('Frame')
+    cv2.namedWindow('Frame') 
 
     df = pd.read_csv(label_fn)
     df.fillna(-1, inplace=True)
@@ -18,26 +25,38 @@ def main(video_fn, label_fn):
 
     ball_pos = []
     # Read until video is completed
+    frame_pos = -1
     while cap.isOpened() and frame_pos + 2 < len(df):
         frame_pos = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+        if frame_pos % 50 == 0:
+            print(frame_pos)
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret is not True:
             break
 
+        if RESIZE and RESIZE != 1:
+            frame = cv2.resize(frame, None, fx=RESIZE, fy=RESIZE)
+
+
         pos = df.loc[frame_pos]
-        if pos['x'] >= 0:
-            cv2.circle(frame, (pos['x'], pos['y']), 9, (0, 0, 255), 8)
+        if SHOW_BALL_LABEL and pos['x'] >= 0:
+            point = (int(pos['x'] * RESIZE), int(pos['y'] * RESIZE))
+            cv2.circle(frame, point, math.ceil(9*RESIZE), (0, 0, 255), math.ceil(8*RESIZE))
 
         # Display the resulting frame
         cv2.imshow('Frame', frame)
 
         # Press Q on keyboard to  exit
-        key = cv2.waitKey(int(25)) & 0xFF
+        key = cv2.waitKey(int(delay)) & 0xFF
         # print(key)
 
         if key == ord('q'):
             break
+        elif key == ord('j'):  # slower
+            delay *= 1.5
+        elif key == ord('k'):  # faster
+            delay /= 1.5
     # When everything done, release the video capture object
     cap.release()
 
