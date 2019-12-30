0fc49d315c62a31f769de4c2b886ecd1624038f5
Marco <mmorik@yahoo.de>
2019-12-30 11:57:26 +0100

    autoencoder learning field but not player

--- v2/autoencoder.py
+++ v2/autoencoder.py
@@ -2,22 +2,24 @@ import os
 
 import numpy as np
 import tensorflow as tf
+import keras
+from keras import backend as K
 import keras.layers as kl
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, Flatten, Conv2DTranspose
 from keras.models import Model
 from keras import backend as K
 from keras.optimizers import Adam
-
+from keras.callbacks import ModelCheckpoint
 import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
-
+import datetime
 from load_images import load_images
 
 
 class Autoencoder():
     """ Small Convolutional Autoencoder"""
 
-    def __init__(self, inp_dim=(128,96,3), state_dim=32, learning_rate=0.005):
+    def __init__(self, inp_dim=(128,96,3), state_dim=32, learning_rate=0.005, loss_weights=False):
         """
         inp_dim:  Dimension of Input feature
         out_dim:  Number of Classes
@@ -38,10 +40,21 @@ class Autoencoder():
         self.autoencoder = Model(self.input_img, self.autoencoder)
 
         self.state_predicter = Model(inputs=self.autoencoder.input, outputs=self.encoder)#self.autoencoder.get_layer("Encoder"))
+        if loss_weights:
+            #assert(np.shape(loss_weights) == inp_dim)
+            #self.loss_weight = tf.constant(np.asarray(loss_weights), dtype=tf.float32)
+            self.loss_weight = tf.keras.backend.placeholder(dtype=tf.float32, shape=inp_dim) #tuple([None] + inp_dim))
 
-        self.autoencoder.compile(optimizer="adam", loss="mse")
+        else:
+            self.loss_weight = tf.constant(np.ones(inp_dim), dtype=tf.float32)
+        self.autoencoder.compile(optimizer="adam", loss=self.weighted_mse)
         self.autoencoder.summary()
         self.state_predicter.summary()
+
+    def weighted_mse(self, y_true, y_pred):
+        std = tf.math.reduce_std(y_true,axis=[0])
+        print(tf.shape(std))
+        return tf.reduce_mean(std * tf.square(y_true - y_pred))
 
     def _make_model(self, l1_reg=0.0001):
         """Creates a Neural Net"""
@@ -81,21 +94,31 @@ class Autoencoder():
         x = UpSampling2D((2, 2))(x)
         #x = kl.BatchNormalization()(x)
         x = Conv2DTranspose(8, (3, 3), activation='relu', padding='same')(x)
-        autoencoder = Conv2DTranspose(3, (3, 3), strides=(2,2), activation='relu', padding='same')(x)
+        autoencoder = Conv2DTranspose(3, (3, 3), strides=(2,2), activation='sigmoid', padding='same')(x)
 
         return encoder, autoencoder
 
-    def train(self, X, Y=None, num_episode=2000, batchsize=150):
+    def train(self, X, Y=None, loss_weights=None, num_episode=300, batchsize=150):
+        """
         self.autoencoder.fit(X, X, batch_size=batchsize, epochs=1)
         result = self.predict(X[:100])
         img = result[0].reshape((128, 96, 3))
         plt.imshow(img)
+        plt.show()
         self.autoencoder.fit(X, X, batch_size=batchsize, epochs=1)
         result = self.predict(X[:100])
         img = result[0].reshape((128, 96, 3))
         plt.imshow(img)
+        """
+        checkpoint = ModelCheckpoint("logs/weights-improvement-{epoch:02d}.hdf5", verbose=1, period=10)
+        logdir = "logs/" + datetime.time().strftime("%Y%m%d-%H%M%S")
+        tensorboard_callback = keras.callbacks.TensorBoard(log_dir=logdir)
 
-        self.autoencoder.fit(X, X, batch_size=batchsize, epochs=num_episode)
+        callbacks_list = [checkpoint, tensorboard_callback]
+
+        # Fit the model
+        self.loss_weight = loss_weights
+        self.autoencoder.fit(X, X, batch_size=batchsize, epochs=num_episode, callbacks=callbacks_list)
 
         # IF you want to save the model
         model_json = self.autoencoder.to_json()
@@ -107,50 +130,50 @@ class Autoencoder():
 
 
     def predict(self, X):
-
         result = self.autoencoder.predict(X, verbose=1, batch_size=100)
-
-
         return result
 
     def predict_states(self, X):
         states = self.state_predicter.predict(X, verbose=1, batch_size=100)
         return states
-    def plot_images_autoencoder(self, orig_imgs, rec_imgs, episode):
+
+    def plot_images_autoencoder(self, orig_imgs, rec_imgs, episode=""):
         plt.close('all')
-        plt.figure(figsize=(8, 2))
+        plt.figure(figsize=(32, 8))
         print(np.shape(orig_imgs))
         print(np.shape(rec_imgs))
         for i in range(8):
 
             plt.subplot(2, 8, i+1)
-            if(self.img_channel==3):
-                plt.imshow(hsv_to_rgb(orig_imgs[i]))
-            else:
-                plt.imshow(orig_imgs[i,:,:,0], cmap='Greys',  interpolation='nearest')
+            plt.imshow(orig_imgs[i])
             plt.axis('off')
 
             plt.subplot(2, 8, i+9)
-            if(self.img_channel ==3):
-                plt.imshow(hsv_to_rgb(rec_imgs[i]))
-            else:
-                plt.imshow(rec_imgs[i,:,:,0], cmap='Greys',  interpolation='nearest')
-
-
+            plt.imshow(rec_imgs[i])
             plt.axis('off')
+
         plt.savefig("autoencodereps{}.png".format(episode))
         plt.pause(0.001)
 
+    def load(self, filename):
+        self.autoencoder.load_weights(filename)
 
 
-def main():
+def main(train=False):
 
     ae = Autoencoder()
+
     X, Y = load_images("output.mp4", "label.csv", 128, 96)
-    ae.train(X,batchsize=100)
-    result = ae.predict(X)
+    if (train):
+        ae.train(X,batchsize=100)
+
+    else:
+        ae.load("logs/weights-improvement-100.hdf5")
+
+    result = ae.predict(X[:100])
     img = result[0].reshape((128, 96, 3))
     plt.imshow(img)
+    for i in range(5):
+        ae.plot_images_autoencoder(X[i*10:(i+1)*10], result[i*10:(i+1)*10], i)
 
-
-main()
+main(False)
