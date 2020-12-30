63c9ec833c15d2578c9d890ae5dc61166ed60fa3
Marco <marco@pop-os.localdomain>
2020-12-30 16:43:42 +0100

    fix videoreader and add ringbuffer

--- labeler.py
+++ labeler.py
@@ -4,6 +4,7 @@ import sys
 import time
 
 import cv2
+from video_reader import VideoReader
 import pandas as pd
 
 # label the video by mouse dragging
@@ -28,12 +29,7 @@ def mousePosition(event, x, y, flags, passed_param):
     elif event == cv2.EVENT_RBUTTONDOWN:
         drop_last = True
 
-def read(cap):
-    # 0-based index of the frame to be decoded/captured next.
-    frame_pos = int(cap.get(cv2.CAP_PROP_POS_MSEC))
-    ret, frame = cap.read()
 
-    return ret, frame, frame_pos
 
 
 
@@ -42,31 +38,30 @@ def label(filename, start_frame=0, ball_pos=None):
     global pos, drop_last, delay, track_point, running
     ball_pos = ball_pos.sort_index()
     # If the input is the camera, pass 0 instead of the video file name
-    cap = cv2.VideoCapture(filename)
 
-    # Check if camera opened successfully
-    if cap.isOpened() == False:
-        print("Error opening video stream or file")
 
     cv2.namedWindow('Frame')
 
     cv2.setMouseCallback('Frame', mousePosition)
-    cap.set(cv2.CAP_PROP_POS_MSEC, start_frame)
+    vr = VideoReader(filename)
+    while vr.next_frame < start_frame:
+        vr.read_next()
 
-    ret, frame, frame_pos = read(cap)
+    frame = vr.read_next()
     cv2.imshow('Frame', frame)
     cv2.waitKey(1600)  # initial wait to position mouse
     #cap.set(cv2.CAP_PROP_POS_MSEC, start_frame)
     # Read until video is completed
-    while(cap.isOpened()):
+    while(vr.is_opened()):
 
         if running:
-            if frame_pos % 50 == 0:
-                print(frame_pos)
+            if vr.next_frame % 50 == 0:
+                print(vr.next_frame)
 
             # Capture frame-by-frame
-            ret, frame, frame_pos = read(cap)
-            if ret is not True:
+            try:
+                frame = vr.read_next()
+            except StopIteration:
                 break
 
         # show last label
@@ -83,18 +78,21 @@ def label(filename, start_frame=0, ball_pos=None):
         if key == ord('q'):
             break
         elif key == ord('u'):
-            ball_pos.loc[frame_pos] ={"x":-1, "y": -1}  # allows overwrite
+            ball_pos.loc[vr.next_frame] ={"x":-1, "y": -1}  # allows overwrite
             ball_pos.sort_index(inplace=True)
-            ball_pos.drop(ball_pos.index[ball_pos.index > frame_pos], inplace=True)
+            ball_pos.drop(ball_pos.index[ball_pos.index > vr.next_frame], inplace=True)
 
         elif key == ord('j'):
             delay *= 1.5
         elif key == ord('k'):  # faster
             delay /= 1.5
-        elif key == ord('h') or key == ord('g'):
-            cap.set(cv2.CAP_PROP_POS_MSEC, max(0,frame_pos - 500 - 1000 *(key == ord('g'))))
-            ret, frame, frame_pos = read(cap)
-            if ret is not True:
+        elif key == ord('h'):
+            vr.jump_back(50)
+        elif key == ord('g'):
+            vr.jump_back(150)
+            try:
+                frame = vr.read_next()
+            except StopIteration:
                 break
             cv2.imshow('Frame', frame)
             cv2.waitKey(100)
@@ -103,25 +101,21 @@ def label(filename, start_frame=0, ball_pos=None):
         if track_point:
             #ball_pos.loc[frame_pos] = [pos[0], pos[1]]
             #ball_pos = ball_pos.sort_index()
-            ball_pos.loc[frame_pos] = {"x": pos[0], "y": pos[1]}  # allows overwrite
+            ball_pos.loc[vr.next_frame] = {"x": pos[0], "y": pos[1]}  # allows overwrite
             ball_pos.sort_index(inplace=True)
-            ball_pos.drop(ball_pos.index[ball_pos.index > frame_pos], inplace=True)
+            ball_pos.drop(ball_pos.index[ball_pos.index > vr.next_frame], inplace=True)
 
         if drop_last and len(ball_pos) > 0:
-            print(f"Last index before drop {ball_pos.index[-1]}")
-            ball_pos = ball_pos.drop(index=ball_pos.index[-1])
-            frame_pos = ball_pos.index[-1]
-            print(f"Last index After drop {ball_pos.index[-1]}")
-
+            next_frame = ball_pos.index[-2]
+            if vr.jump_back(vr.next_frame - next_frame):
+                ball_pos = ball_pos.drop(index=ball_pos.index[-1])
+            else:
+                print('Last point is too much time ago to be deleted')
             try:
-                cap.set(cv2.CAP_PROP_POS_MSEC, frame_pos)
-            except IndexError:
-                cap.set(cv2.CAP_PROP_POS_MSEC, 0)
-            #print("Dropped the last Frame with index",  int(cap.get(cv2.CAP_PROP_POS_MSEC)))
-
-            ret, frame, frame_pos = read(cap)
-            if ret is not True:
+                frame = vr.read_next()
+            except StopIteration:
                 break
+
             cv2.imshow('Frame', frame)
             cv2.waitKey(100)
 
@@ -130,7 +124,7 @@ def label(filename, start_frame=0, ball_pos=None):
 
 
     # When everything done, release the video capture object
-    cap.release()
+    vr.cap.release()
 
     # Closes all the frames
     cv2.destroyAllWindows()
