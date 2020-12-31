438a091ec8981b7adf25beda2e88460150c10abb
Marco <marco@pop-os.localdomain>
2020-12-31 14:49:14 +0100

    some more basic tracking stuf

--- objecttracking.py
+++ objecttracking.py
@@ -40,8 +40,11 @@ def get_image(video):
     mask = cv2.inRange(hsv, lower_b, upper_b)
     # Bitwise-AND mask and original image
     frame = cv2.bitwise_and(frame,frame, mask= mask)
+    scale=.2
+    frame = cv2.resize(frame,(int(1280*scale),int(720*scale)))
     return frame
-if __name__ == '__main__':
+
+def try_tracking():
 
     # Set up tracker.
     # Instead of MIL, you can also use
@@ -106,7 +109,7 @@ if __name__ == '__main__':
 
         # Display result
         cv2.imshow("Tracking", frame)
-
+        cv2.resizeWindow('Tracking', 1280, 720)
         # Exit if ESC pressed
         k = cv2.waitKey(1) & 0xff
         if k == 27: break
@@ -119,3 +122,81 @@ if __name__ == '__main__':
             _ = tracker.init(frame, bbox)
 
             _ , bbox = tracker.update(frame)
+
+def get_optical_flow_edges():
+    cap = cv2.VideoCapture("dataset/v2.h265")
+    # params for ShiTomasi corner detection
+    feature_params = dict(maxCorners=100,
+                          qualityLevel=0.3,
+                          minDistance=7,
+                          blockSize=7)
+
+    for i in range(540):
+        ok, frame = cap.read()
+    # Parameters for lucas kanade optical flow
+    lk_params = dict(winSize=(15, 15),
+                     maxLevel=2,
+                     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+    # Create some random colors
+    color = np.random.randint(0, 255, (100, 3))
+    # Take first frame and find corners in it
+    ret, old_frame = cap.read()
+    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+
+    bbox = cv2.selectROI(frame, False)
+    print(bbox)
+    p0 = cv2.goodFeaturesToTrack(old_gray, mask=None, **feature_params)
+    print(p0.shape)
+    p0[-1] = [[bbox[0],bbox[1]]]
+    p0[-2] = [[bbox[0]+bbox[2],bbox[1]+bbox[3]]]
+    print(p0)
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(old_frame)
+    while (1):
+        ret, frame = cap.read()
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+        # Select good points
+        good_new = p1[st == 1]
+        good_old = p0[st == 1]
+        # draw the tracks
+        for i, (new, old) in enumerate(zip(good_new, good_old)):
+            a, b = new.ravel()
+            c, d = old.ravel()
+            mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
+            frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+        img = cv2.add(frame, mask)
+        cv2.imshow('frame', img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+        # Now update the previous frame and previous points
+        old_gray = frame_gray.copy()
+        p0 = good_new.reshape(-1, 1, 2)
+
+def get_optical_flow_all():
+    cap = cv2.VideoCapture("dataset/v2.h265")
+    ret, frame1 = cap.read()
+    prvs = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    hsv = np.zeros_like(frame1)
+    hsv[..., 1] = 255
+    while (1):
+        ret, frame2 = cap.read()
+        next = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+        flow = cv2.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+        hsv[..., 0] = ang * 180 / np.pi / 2
+        hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+        bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        cv2.imshow('frame2', bgr)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+        elif k == ord('s'):
+            cv2.imwrite('opticalfb.png', frame2)
+            cv2.imwrite('opticalhsv.png', bgr)
+        prvs = next
+if __name__ == '__main__':
+    #get_optical_flow_edges()
+    try_tracking()
